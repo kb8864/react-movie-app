53b0e2300d7731a40fcf1b083203ffb6424d2317
kb8864 <yt7s858864@gmail.com>
2025-09-27 19:50:49 +0900

    typeScripの型を利用する

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 
 import './App.css'
 import { useState, useEffect } from 'react';
+
+type Movie ={
+  id: number;
+  original_title: string;
+  poster_path: string;
+  overview: string;
+}
+
+// APIから返ってくる情報
+type MovieJson = {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
 function App() {
   // .envで設定した環境変数を読み込む
     const apiKey = import.meta.env.VITE_TMDB_API_KEY;
@@ -50,7 +76,7 @@ const fetchMovieList =async() => {
 }
 
 const [keyword,setKeyword] = useState("");
-const [movieList, setMovieList] = useState([]);
+const [movieList, setMovieList] = useState<Movie[]>([]);
   useEffect(() => {
     fetchMovieList();
   }, []);
